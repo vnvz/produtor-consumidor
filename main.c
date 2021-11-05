33ad014a3931e0514eafcb73dff15075cfbcfa6b
vnvz <vvv@cesar.school>
2021-11-05 18:09:15 -0300

    reescrever algumas funções e adicionar um timer de finalização

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <semaphore.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define MAX_ITEM 5
 #define BUFFER 5
@@ -12,81 +12,78 @@ sem_t cheio;
 
 int in = 0;
 int out = 0;
-int buffer[BUFFER];
+int buffer_arr[BUFFER];
 pthread_mutex_t mutex;
 
 void *insert_item(void *ino)
-{
+{   
     int data;
-    for(int i = 0; i < MAX_ITEM; i++){
+    for(int i = 0; i < MAX_ITEM; i++) {
         data = rand();
-        sem_wait(&vazio);           // Funções respectivas
-        pthread_mutex_lock(&mutex); // de espera
+        sem_wait(&vazio);           // Funções de espera
+        pthread_mutex_lock(&mutex);
+
+        buffer_arr[in] = data;      // Inserir o dado no buffer e printar info sobre ele
+        printf("Produtor %d: Inserir dado %d em %d\n", *((int *)ino), buffer_arr[in], in);
+
+        in = (in + 1) % BUFFER;     // Sinalizar o consumidor que o buffer está cheio
+        pthread_mutex_unlock(&mutex);
+        sem_post(&cheio);
+    }
+}
+void *remove_item(void *cno)
+{   
+    for(int i = 0; i < MAX_ITEM; i++) {
+        sem_wait(&cheio);           // Funções de espera
+        pthread_mutex_lock(&mutex);
         
-        buffer[in] = data;
-        printf("Produtor %d: Inserir dado %d em %d\n", 
-        *((int *)ino),              // Inserir o dado no buffer
-        buffer[in], in);            // e printar info sobre ele
+        int data = buffer_arr[out]; // Remover dado do buffer e printar info sobre ele
+        printf("Consumidor %d: Remover dado %d em %d\n",*((int *)cno), data, out);
 
-        in = (in + 1) % BUFFER;     
-        pthread_mutex_lock(&mutex); // Sinalizar ao consumidor
-        sem_post(&cheio);           // que o buffer está cheio
+        out = (out + 1) % BUFFER;   // Sinalizar ao produtor que o buffer está vazio
+        pthread_mutex_unlock(&mutex);
+        sem_post(&vazio);
     }
 }
 
-void *remove_item(void *rno)
-{
-    for(int i = 0; i < MAX_ITEM; i++){
-        sem_wait(&cheio);           // Funções respectivas
-        pthread_mutex_lock(&mutex); // de espera
-
-        int data = buffer[out];
-        printf("Consumidor %d: Remover dado %d em %d\n",
-        *((int *)rno),              // Remover o dado do buffer
-        data, out);                 // e printar info sobre ele
-
-        out = (out + 1) % BUFFER;
-        pthread_mutex_unlock(&mutex); // Sinalizar ao produtor
-        sem_post(&vazio);             // que o buffer está vazio
-    }
-}
-
-int main(int argc, int* argv[])
-{
+int main()
+{   
     pthread_t produtor[5], consumidor[5]; // Definição dos tipos
     pthread_mutex_init(&mutex, NULL);     // Inicializar o mutex
-    
+
     sem_init(&vazio, 0, BUFFER);          // Inicializar os semáforos
     sem_init(&cheio, 0, 0);               // de vazio e cheio
 
-    int sleept = &argv[1];                 // Tempo para a função dormir antes de encerrar
-    int nump = &argv[2];                   // Adiquirir o produtor e consumidor
-    int numc = &argv[3];                   // através do argv[2] e argv[3]
+    int numpc[5] = {1,2,3,4,5};
+    int sleept;
+    printf("Esse programa demonstrará 5 produtores e 5 consumidores.\n");
+    printf("Por quanto tempo o programa deverá dormir após a execução?\n");
+    scanf("%d", &sleept);
 
-    printf("sleept: %d\n nump: %d\n numc: %d\n", sleept, nump, numc);
+    for(int i = 0; i < 5; i++) {
+        pthread_create(&produtor[i], NULL, (void *)insert_item, (void *)&numpc[i]);
+    } // Cria a thread do produtor]
 
-    for(int i = 0; i < 5; i++){
-        pthread_create(&produtor[i], NULL, (void *)insert_item, (void *)&nump);
-    } // Cria a thread do produtor
-
-    for(int i = 0; i < 5; i++){
-        pthread_create(&consumidor[i], NULL, (void *)remove_item, (void *)&numc);
+    for(int i = 0; i < 5; i++) {
+        pthread_create(&consumidor[i], NULL, (void *)remove_item, (void *)&numpc[i]);
     } // Cria a thread do consumidor
 
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < 5; i++) {
         pthread_join(produtor[i], NULL); // Prevenção de deadlock
     }
-
-    for(int i = 0; i < 5; i++){
-        pthread_join(consumidor[i], NULL);  // Prevenção de deadlock
+    for(int i = 0; i < 5; i++) {
+        pthread_join(consumidor[i], NULL); // Prevenção de deadlock
     }
 
-    printf("Programa adormecendo por %d segundos...", sleept);
-    sleep(sleept);
+    printf("\n\nO programa irá adormecer por %d segundos", sleept);
+    printf("\nBoa noite...");
+    printf("\nBom dia! Até mais!");
 
-    pthread_mutex_destroy(&mutex);  // Limpeza do lixo
+    pthread_mutex_destroy(&mutex); // Limpeza do lixo
     sem_destroy(&vazio);
     sem_destroy(&cheio);
+
+    sleep(sleept);
 
     return 0;
 }
